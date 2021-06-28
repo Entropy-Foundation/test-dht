03955567d78d2d8f60d35691f37536acb65e017f
Oluseun Aminu <seun.aminujoshua@gmail.com>
2021-06-28 20:06:45 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,56 @@
+use async_std::future;
 use futures::executor::block_on;
-use futures::prelude::*;
-use libp2p::core::{transport, muxing, multiaddr};
+
+use futures::{task, executor, stream::once};
+use futures::future::{ok, ready, Ready};
+use libp2p::core::transport::MemoryTransport;
+use libp2p::core::{Executor, transport, muxing, multiaddr::{Protocol, Multiaddr}};
 use libp2p::kad::record::store::MemoryStore;
-use libp2p::kad::{
-    AddProviderOk,
-    Kademlia,
-    KademliaEvent,
-    kbucket::KBucketsTable,
-    PeerRecord,
-    PutRecordOk,
-    QueryResult,
-    Quorum,
-    Record,
-    record::Key,
-};
-use libp2p::{NetworkBehaviour, Multiaddr};
-use libp2p::swarm::NetworkBehaviourEventProcess;
+use libp2p::kad::{Kademlia, KademliaEvent};
+use libp2p::swarm::ExpandedSwarm;
 use libp2p::{PeerId, development_transport, identity, Transport};
 use libp2p::ping::{Ping, PingConfig};
-use libp2p::swarm::Swarm;
-use libp2p_kad::{QueryId, QueryInfo, kbucket, Kademlia as SKademlia};
+use libp2p::swarm::{Swarm, SwarmBuilder};
 use sc_network::{NetworkWorker, DhtEvent};
-use std::{error::Error, env, task::Poll, fmt, net::Ipv4Addr};
-use async_std::{io, task};
+use std::{env, task::Poll, fmt, net::Ipv4Addr};
+use actix_web::{get, web, App, Error, HttpResponse, HttpServer};
 
-// #[async_std::main]
 fn main() {
     env_logger::init();
 
-    find_nodes();
+    block_on(discover_peer());
+    // println!("{:?}", multi_addr);
+
+    
 }
 
-fn find_nodes() -> Result<(), Box<dyn Error>> {
-    // Create a random key for ourselves.
+async fn discover_peer() {
+    // identity
     let local_key = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(local_key.public());
-    let localhost = Ipv4Addr::new(127, 0, 0, 1);
-
-    println!("Local peer id: {}", local_peer_id);
-
-    let transport = block_on(development_transport(local_key))?;
-
-    let store = MemoryStore::new(local_peer_id.clone());
-    let mut kademlia = Kademlia::new(local_peer_id.clone(), store);
-    kademlia.get_closest_peers(local_peer_id);
-
-    let mut behaviour = SupraBehaviour {
-        kademlia: kademlia
-    };
-    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
-
-    let test_tcp: &str = "4242";
-    let targetted_tcp = multiaddr::Protocol::Ip4(localhost);
-
-    // refernece: https://docs.libp2p.io/concepts/addressing/
+    let local_peer_id = PeerId::from_public_key(local_key.public().clone());
+    println!("Local peer id: {:?}", &local_peer_id);
+    
+    
+    //address
+    let localhost = Ipv4Addr::new(0, 0, 0, 0);
+    let test_tcp: &str = "0";
+    let targetted_tcp = Protocol::Ip4(localhost);
+    
     let address = format!("{}/tcp/{}/p2p/{}", targetted_tcp, test_tcp, local_peer_id.clone());
+    let multi_addr: Multiaddr = address.parse().unwrap();
+    println!("{}", multi_addr);
 
-    println!("{}", address);
 
-    swarm.listen_on(address.parse().unwrap())?;
+    // storage
+    let store = MemoryStore::new(local_peer_id.clone());
+    let mut kademlia = Kademlia::new(local_peer_id, store);
 
-    Ok(())
-}
+    let transport = block_on(development_transport(local_key)).unwrap();
+    let mut swarm = Swarm::new(transport, kademlia, local_peer_id);
 
-fn announce_peer() {
-    // args: id, info_hash, port, opaque_tokens
-}
 
-fn ping() {
+    // swarm.listen_on(multi_addr.clone()).await;
+    let event = swarm.behaviour_mut();
 
-}
-
-#[derive(NetworkBehaviour)]
-pub struct SupraBehaviour {
-    kademlia: Kademlia<MemoryStore>
-}
-
-impl NetworkBehaviourEventProcess<KademliaEvent> for SupraBehaviour {
-    fn inject_event(&mut self, event: KademliaEvent) {
-        todo!()
-        //add a ping event. Ref: https://docs.rs/libp2p/0.38.0/libp2p/ping/struct.Ping.html
-        //use match so other forms of event can be considered!
-        // for response to event, we can use sc_network DhtEvent lib: https://docs.rs/sc-network/0.9.0/sc_network/enum.DhtEvent.html
-    }
-}
-
-pub struct SupraTransport {
-
+    
 }
